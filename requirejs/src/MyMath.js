15f483862ea1805c4410b43819dbfc2e7e451b55
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-04-20 18:09:10 +0400

    MyMath test

---
+++ requirejs/src/MyMath.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dmitry on 20.04.16.
+ */
+define(function(){
+
+    //simple adding
+    function _add(a, b){
+        return a+b;
+    }
+
+    return {
+        add: _add
+    }
+});
--- requirejs/src/main.js
+++ requirejs/src/main.js
@@ -1,3 +1,7 @@
 /**
  * Created by dmitry on 17.04.16.
  */
+
+require(['MyMath'], function(MyMath) {
+     console.log(MyMath.add(4, 3));
+});
