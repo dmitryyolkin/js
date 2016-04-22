97c41a11fd7bd3bbe334e9918cbfb5a27c8456c4
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-04-22 18:03:43 +0300

    add routing

--- requirejs/src/main.js
+++ requirejs/src/main.js
@@ -2,12 +2,12 @@
  * Created by dmitry on 17.04.16.
  */
 
-require(['models/User', 'controllers/ListController'], function(User, ListController){
+require(['models/User', 'Router'], function(User, Router){
      var users = [];
      for (var i = 0; i < 3; i++){
           users[i] = new User('user' + i);
      }
 
      localStorage.users = JSON.stringify(users);
-     ListController.start();
+     Router.startRouting();
 });
