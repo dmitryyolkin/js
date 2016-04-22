731ae7c127f9e0d1941cf9bcadb6f98b7603ec74
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-04-22 16:40:55 +0300

    show list of users

--- requirejs/src/main.js
+++ requirejs/src/main.js
@@ -2,6 +2,12 @@
  * Created by dmitry on 17.04.16.
  */
 
-require(['MyMath'], function(MyMath) {
-     console.log(MyMath.add(4, 3));
+require(['models/User', 'controllers/ListController'], function(User, ListController){
+     var users = [];
+     for (var i = 0; i < 3; i++){
+          users[i] = new User('user' + i);
+     }
+
+     localStorage.users = JSON.stringify(users);
+     ListController.start();
 });
--- requirejs/src/views/ListView.js
+++ requirejs/src/views/ListView.js
@@ -1,3 +1,21 @@
 /**
  * Created by yolkin on 20.04.2016.
  */
+
+define(function(){
+
+    function _render(users){
+        var appDiv = document.getElementById('app');
+        var html = '<ul>';
+        for (var i = 0; i < users.length; i++){
+            html += '<li>' + users[i].name + '</li>';
+        }
+        html += '</ul>';
+        appDiv.innerHTML = html;
+    }
+
+    return {
+        render: _render
+    };
+
+});
