caf53b9f1414544e6b0c5ec9ed72438a2d933915
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-06-09 08:16:07 +0400

    Change GruntFile.js to compile one result  file only

--- mvc/backbone_example/Gruntfile.js
+++ mvc/backbone_example/Gruntfile.js
@@ -3,76 +3,43 @@ module.exports = function(grunt) {
     'use strict';
 
     //variables
-    var scrDir = 'static/client/';
-    var buildDir = 'static/client/build';
+    var srcDir = 'static/client/';
+    var buildDir = 'static/client/';
 
     // Project configuration.
     grunt.initConfig({
         // Metadata.
         pkg: grunt.file.readJSON('package.json'),
 
-        //clean build directory
-        clean: [buildDir],
-
         requirejs: {
             //All possible values can be found here
             //https://github.com/requirejs/r.js/blob/master/build/example.build.js
             options: {
-                'appDir': scrDir,
-                'dir': buildDir,
-                'mainConfigFile': scrDir + 'config.js',
-                'optimize': 'none'
+
+                mainConfigFile: srcDir + 'config.js',
+                out: buildDir + 'main-single.js',
+
+                optimize: 'none'
                 //'optimize': 'uglify2',
-                //'normalizeDirDefines': 'skip',
-                //'skipDirOptimize': true,
             },
             centralized: {
-                options: {
-                    //modules specifies what module should be optimized
-                    //in name we specify name of JS file
+                //TODO it's very useful
+                //if we use modules option (see examples in my github https://github.com/dmitryyolkin/js/tree/master/requirejs)
+                //then we have to use 'appDir' and 'dir' attributes and DON'T use out because in this case we optimize whole directory (but not a separate file)
 
+                //if we use 'baseUrl' and 'out' attributes then we optimize one single file only
+                //we don't optimize whole directory
+
+                options: {
                     //As a result we have one main,js file including all files in itself in build directory
-                    //All other files can be removed
-                    'modules': [{
-                        'name': 'main',
-                        'include': [
-                            'jquery',
-                            'underscore',
-                            'backbone'
-                        ]
-                    }]
-                }
-            },
+                    include: ['config', 'main'],
 
-            centralizedAlmond: {
-                //almond is small AMD (Asynchronious Model Definition) loader that is much smaller than requirejs
-                //and typically included at the same output file
-
-                //Useful links:
-                //https://github.com/requirejs/almond
-                //http://www.codechewing.com/library/optimise-requirejs-almond-grunt/
-
-                options: {
-                    //As a result we have one main.js file including all files in itself in build directory
-                    //All other files can be removed
-
-                    //In addition almond.js in included in main.js as well and require.js can be removed from build directory as well
-                    almond: true,
-
-                    'modules': [{
-                        'name': 'main',
-                        'include': [
-                            'jquery',
-                            'underscore',
-                            'backbone',
-
-                            //if config is not included in final file then almond configuration doesn't work
-                            'config'
-                        ]
-                    }]
+                    //The name parameter is pointing to almond, relative to our baseUrl, which will include almond in the build process.
+                    //almond is small AMD (Asynchronious Model Definition) loader that is much smaller than requirejs
+                    //and typically included at the same output file
+                    name: '../components/almond/almond'
                 }
             }
-
         }
 
     });
@@ -80,19 +47,10 @@ module.exports = function(grunt) {
     // These plugins provide necessary tasks.
     //grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    //clean build directory
-    grunt.loadNpmTasks('grunt-contrib-clean');
-
     //grunt.loadNpmTasks('grunt-contrib-requirejs');
-    //grunt-requirejs is much better becasue it contains internally almond,js and other modules
-    //if you want to compile with almond please use grunt-requirejs
-    //if you don't need almond you can use grunt-contrib-requirejs
-    grunt.loadNpmTasks('grunt-requirejs');
-
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
 
     // Default task.
     //grunt.registerTask('default', ['clean', 'requirejs:centralized']);
-    grunt.registerTask('default', ['clean', 'requirejs:centralizedAlmond']);
-    //grunt.registerTask('default', ['clean', 'requirejs:independent', 'string-replace', 'copy']);
-    //grunt.registerTask('default', ['clean', 'requirejs:shared']);
+    grunt.registerTask('default', ['requirejs:centralized']);
 };
