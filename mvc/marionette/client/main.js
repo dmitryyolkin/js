18150f473780c1f10c67684dcfd5553dac546412
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-07-22 08:42:26 +0400

    marionette
    * test some Marionette functionality like Application, Events, AppRouter

--- mvc/marionette/client/main.js
+++ mvc/marionette/client/main.js
@@ -1,3 +1,44 @@
 /**
  * Created by dmitry on 06.07.16.
  */
+
+'use strict';
+
+define(function(require){
+    //libs
+    var Backbone = require('backbone');
+    var Marionette = require('marionette');
+
+    //internal
+    var AppRouter = require('./AppRouter');
+
+    var App = new Marionette.Application();
+
+    //add initializers
+    App.addInitializer(function(options){
+        new AppRouter();
+        Backbone.history.start();
+    });
+
+    //embedded events
+    App.on('initialize:after', function(options){
+        if (Backbone.history){
+            Backbone.history.start();
+        }
+    });
+
+    App.on('start', function(options){
+        alert('start: ' + options);
+    });
+
+    //something useful can be put in options
+    var options = {};
+    App.start(options);
+
+    //EventAggregator
+    App.vent.on('Dmitry_test', function(){
+        console.log("it's dmitry's test event");
+    });
+    App.vent.trigger('Dmitry_test');
+
+});
