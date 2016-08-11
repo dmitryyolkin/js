bef7840674dc8c610c08090f8e98d075c529e024
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-08-11 08:26:58 +0400

    cajon_almond_requirejs_grunt
    * init

---
+++ templates/cajon_almond_grunt/client/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by dmitry on 06.07.16.
+ */
+
+'use strict';
+
+//var App = require('./App');
+
+//require jquery is for example and be able to invoke $(document).ready(main) from index.hbs
+//if we use backbone or jquery then his require is not needed
+var $ = require('jquery');
+
+module.exports = function(){
+    console.log('main is started');
+};
