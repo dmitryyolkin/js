b5aad5fa77a1718b5487d9146dbcf4b9f3dc2d89
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-10-28 08:11:36 +0400

    exclude possibility of double initialization

--- zoo/server/testDataProvider.js
+++ zoo/server/testDataProvider.js
@@ -68,53 +68,7 @@ module.exports = {
 
     //create temp Zoo
     createZoo: function () {
-        //create Cages
-        var predator1Cage = new Cage({name: 'predators1'});
-        var fish1Cage = new Cage({name: 'fishes1'});
-        saveInstances(
-            [predator1Cage, fish1Cage],
-            function (cage) {
-                return {
-                    name: cage.name
-                };
-            },
-            Cage
-        );
-
-        //create keepers and zoologist
-        var keeper1 = creareZooWorker({name: 'keeper1', roles: ['KEEPER']});
-        var keeper2 = creareZooWorker({name: 'keeper2', roles: ['KEEPER']});
-        var zoologist1 = creareZooWorker({name: 'zoologist1', roles: ['ZOOLOGIST']});
-
-        saveInstances(
-            [keeper1, keeper2, zoologist1],
-            function (user) {
-                return {
-                    login: user.login
-                };
-            },
-            User
-        );
-
-        //create Animals
-        var animals = [];
-        animals.push(new Animal({name: 'lion', species: 'Predator', age: 10, cage: predator1Cage._id, keeper: keeper1._id}));
-        animals.push(new Animal({name: 'tiger', species: 'Predator', age: 20, cage: predator1Cage._id, keeper: keeper1._id}));
-        animals.push(new Animal({name: 'shark', species: 'Fish', age: 30, cage: fish1Cage._id, keeper: keeper2._id}));
-
-        saveInstances(
-            animals,
-            function (animal) {
-                return {
-                    name: animal.name
-                };
-            },
-            Animal
-        );
-
-        //post-saving
-        //cages
-        function filterAnimals(species) {
+        function filterAnimals(animals, species) {
             return animals.filter(function (animal) {
                 return animal.species == species;
             });
@@ -126,74 +80,124 @@ module.exports = {
             }
         }
 
-        predator1Cage.animals = filterAnimals('Predator');
-        fish1Cage.animals = filterAnimals('Fish');
-
-        var cages = [];
-
-        checkAnimalsInCage(cages, predator1Cage);
-        checkAnimalsInCage(cages, fish1Cage);
-        for (var i = 0; i < cages.length; i++) {
-            cages[i].save(function(err, cage){
-                if (err){
-                    logger.error(err);
-                    return;
-                }
-                logger.info('Cage was updated: ' + JSON.stringify(cage, null, "\t"));
-            })
-        }
-
-        //users
-        var keepers = [];
-        checkAnimalsInCage(keepers, keeper1);
-        checkAnimalsInCage(keepers, keeper2);
-        for (var i = 0; i < keepers.length; i++) {
-            var keeper = keepers[i];
-            for (var j = 0; j < animals.length; j++) {
-                var animal = animals[j];
-                if (animal.keeper == keeper._id){
-                    if (keeper.animals.length == 0){
-                        keeper.animals = [];
-                    }
-                    keeper.animals.push(animal);
-                }
-            }
-
-            //save
-            keeper.save(function(err, doc){
-                if (err){
-                    logger.error(err);
-                    return;
-                }
-                logger.info('Keeper was updated: ' + JSON.stringify(doc, null, "\t"));
-            });
-
-        }
-
-
-        //create Zoo
+        //check whether initialization was already done or not
         Zoo.findOne(
             {},
-            function(err, zoo){
+            function(err, mongoZoo){
                 if (err){
                     logger.error(err);
                     return;
                 }
 
-                if (!zoo){
-                    new Zoo({
-                        users: [keeper1, keeper2, zoologist1],
-                        animals: animals
-                    }).save(function(err, zoo){
+                if (mongoZoo){
+                    logger.info('Instance of Zoo was already saved before:' + JSON.stringify(mongoZoo, null, "\t"));
+                    return;
+                }
+
+                //initialize zoo
+                //create Cages
+                var predator1Cage = new Cage({name: 'predators1'});
+                var fish1Cage = new Cage({name: 'fishes1'});
+                saveInstances(
+                    [predator1Cage, fish1Cage],
+                    function (cage) {
+                        return {
+                            name: cage.name
+                        };
+                    },
+                    Cage
+                );
+
+                //create keepers and zoologist
+                var keeper1 = creareZooWorker({name: 'keeper1', roles: ['KEEPER']});
+                var keeper2 = creareZooWorker({name: 'keeper2', roles: ['KEEPER']});
+                var zoologist1 = creareZooWorker({name: 'zoologist1', roles: ['ZOOLOGIST']});
+
+                saveInstances(
+                    [keeper1, keeper2, zoologist1],
+                    function (user) {
+                        return {
+                            login: user.login
+                        };
+                    },
+                    User
+                );
+
+                //create Animals
+                var animals = [];
+                animals.push(new Animal({name: 'lion', species: 'Predator', age: 10, cage: predator1Cage._id, keeper: keeper1._id}));
+                animals.push(new Animal({name: 'tiger', species: 'Predator', age: 20, cage: predator1Cage._id, keeper: keeper1._id}));
+                animals.push(new Animal({name: 'shark', species: 'Fish', age: 30, cage: fish1Cage._id, keeper: keeper2._id}));
+
+                saveInstances(
+                    animals,
+                    function (animal) {
+                        return {
+                            name: animal.name
+                        };
+                    },
+                    Animal
+                );
+
+                //post-saving
+                //cages
+                predator1Cage.animals = filterAnimals(animals, 'Predator');
+                fish1Cage.animals = filterAnimals(animals, 'Fish');
+
+                var cages = [];
+
+                checkAnimalsInCage(cages, predator1Cage);
+                checkAnimalsInCage(cages, fish1Cage);
+                for (var i = 0; i < cages.length; i++) {
+                    cages[i].save(function(err, cage){
                         if (err){
                             logger.error(err);
+                            return;
                         }
-                        logger.info('Zoo was saved: ' + JSON.stringify(zoo, null, "\t"));
+                        logger.info('Cage was updated: ' + JSON.stringify(cage, null, "\t"));
                     })
                 }
+
+                //users
+                var keepers = [];
+                checkAnimalsInCage(keepers, keeper1);
+                checkAnimalsInCage(keepers, keeper2);
+                for (var i = 0; i < keepers.length; i++) {
+                    var keeper = keepers[i];
+                    for (var j = 0; j < animals.length; j++) {
+                        var animal = animals[j];
+                        if (animal.keeper == keeper._id){
+                            if (keeper.animals.length == 0){
+                                keeper.animals = [];
+                            }
+                            keeper.animals.push(animal);
+                        }
+                    }
+
+                    //save
+                    keeper.save(function(err, doc){
+                        if (err){
+                            logger.error(err);
+                            return;
+                        }
+                        logger.info('Keeper was updated: ' + JSON.stringify(doc, null, "\t"));
+                    });
+
+                }
+
+
+                //create Zoo
+                new Zoo({
+                    users: [keeper1, keeper2, zoologist1],
+                    animals: animals
+                }).save(function(err, zoo){
+                    if (err){
+                        logger.error(err);
+                    }
+                    logger.info('Zoo was saved: ' + JSON.stringify(zoo, null, "\t"));
+                });
             }
         );
-
 
     }
 
