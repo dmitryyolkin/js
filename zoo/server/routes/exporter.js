aa5cbb506a34427d5737d4ea2998203d2518a3c9
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2016-12-26 08:29:17 +0400

    Support excel export with node-excel-export

--- zoo/server/routes/exporter.js
+++ zoo/server/routes/exporter.js
@@ -5,6 +5,7 @@
 
 var express = require('express');
 var json2csv = require('json2csv');
+var excel = require('node-excel-export');
 
 var mongoModels = require('../schema/MongoModels');
 
@@ -76,7 +77,109 @@ router.get('/csv/', function (req, res, next) {
 router.get('/excel/', function (req, res, next) {
     console.log('get /export/excel');
     exportAnimals(function (animals) {
-        //todo export in excel
+
+        //We can define styles as json object
+        //More info: https://github.com/protobi/js-xlsx#cell-styles
+        var styles = {
+            headerDark: {
+                fill: {
+                    fgColor: {
+                        rgb: 'FF000000'
+                    }
+                },
+                font: {
+                    color: {
+                        rgb: 'FFFFFFFF'
+                    },
+                    sz: 14,
+                    bold: true,
+                    underline: true
+                }
+            },
+            cellPink: {
+                fill: {
+                    fgColor: {
+                        rgb: 'FFFFCCFF'
+                    }
+                }
+            },
+            cellGreen: {
+                fill: {
+                    fgColor: {
+                        rgb: 'FF00FF00'
+                    }
+                }
+            }
+        };
+
+        //Here we specify the export structure
+        var specification = {
+            // <- the key should match the actual data key
+            name: {
+                displayName: 'Animal name', // <- Here you specify the column header
+                headerStyle: styles.headerDark, // <- Header style
+                //cellStyle: styles.cellPink,
+                width: 120 // <- width in pixels
+            },
+            species: {
+                displayName: 'Species',
+                headerStyle: styles.headerDark,
+                cellStyle: function(value, row) {
+                    return (value == 'Predator') ? styles.cellPink : styles.cellGreen;
+                },
+                width: 120
+            },
+            age: {
+                displayName: 'Age',
+                headerStyle: styles.headerDark,
+                //cellStyle: styles.cellPink, // <- Cell style
+                width: 50
+            },
+            'keeper': {
+                displayName: 'Keeper',
+                headerStyle: styles.headerDark,
+                cellStyle: styles.cellGreen,
+                width: 120
+            },
+            'cage': {
+                displayName: 'Cage',
+                headerStyle: styles.headerDark,
+                //cellStyle: styles.cellGreen,
+                width: 120
+            }
+        };
+
+        // The data set should have the following shape (Array of Objects)
+        // The order of the keys is irrelevant, it is also irrelevant if the
+        // dataset contains more fields as the report is build based on the
+        // specification provided above. But you should have all the fields
+        // that are listed in the report specification
+        var dataset = animals.map(function(animal){
+            return {
+                name: animal.name,
+                species: animal.species,
+                age: animal.age,
+                keeper: animal.keeper.name,
+                cage: animal.cage.name
+            }
+        });
+
+        // Create the excel report.
+        // This function will return Buffer
+        var report = excel.buildExport(
+            [ // <- Notice that this is an array. Pass multiple sheets to create multi sheet report
+                {
+                    name: 'Animals', // <- Specify sheet name (optional)
+                    specification: specification, // <- Report specification
+                    data: dataset // <-- Report data
+                }
+            ]
+        );
+
+        //We can then return this straight
+        //This is sails.js specific (in general you need to set headers)
+        res.attachment('animals_table.xlsx');
+        return res.send(report);
     });
 });
 
