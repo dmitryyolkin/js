3d1b07f69fb061870f2c40d82f59d7002a1eda9e
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2017-07-05 08:55:49 +0400

    React component usages

--- mvc/react-app/src/App.js
+++ mvc/react-app/src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function TestFuncComponent(prop){
+    return <b>{prop.name}</b>;
+}
+
+class TestClassComponent extends Component {
+    render() {
+        return <i>{this.props.freeFormAttr}</i>;
+    }
+}
+
 class App extends Component {
   render() {
     return (
@@ -9,7 +19,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React </h2>
+
+            <TestFuncComponent name="test: TestFuncComponent-name"/>
+            <TestClassComponent freeFormAttr="test: TestClassComponent-freeFormAttr"/>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
