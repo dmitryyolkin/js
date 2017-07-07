4ea896b05fde1d49c67356d99f4ef44cb9fa8d95
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2017-07-07 08:12:59 +0400

    create Toggle component * example of event handler

---
+++ mvc/react-app/src/components/Toggle.js
@@ -0,0 +1,41 @@
+/**
+ * Created by dmitry on 07.07.17.
+ */
+'use strict';
+
+import React from 'react';
+
+class Toggle extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isToggleOn: false
+        };
+
+        //it's needed to be able to use this within handleClick
+        //Будьте осторожны с этими значениями в обратных сигналах JSX.
+        //Классовые алгоритмы не связаны по умолчанию.
+        //Если вы забыли связать их, выполните this.handleClick и передайте его на onClick.
+        // This станет undefined когда функция получит сигнал.
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn : !prevState.isToggleOn
+        }))
+    }
+
+    render() {
+        return (
+            //link to method handler should be done in camelCase
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? "ON" : "OFF"}
+            </button>
+        );
+    }
+
+}
+
+export default Toggle;
